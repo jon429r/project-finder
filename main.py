19f3fb42a72d162ebebb5fddf75d9264185c03c2
jon429r <jonathanday088@gmail.com>
2023-11-29 14:28:55 -0500

    commit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
     use this to implement the open command for internet links
     maybe include a column in the table to include links to project websites
     aka projects that dont have physical directories on the local computer
+
+    can also open vs code with the command: os.system('open -a "Visual\ Studio\ Code"')
 '''
 
 import sqlite3
@@ -22,6 +24,8 @@ def help_command():
     List of possible commands: \n
     1. Create a new project: new -name <"project_name"> -dir <"working_directory"> \n
     please insert the whole directory path starting with a '/'
+    Additional arguments: \n
+    -link <"project_link"> \n
     2. View existing projects: todo \n
     3. Finished an existing project: finish -name <project_name> \n
     You can also remove a project by typing finish -id <project_id> \n
@@ -35,12 +39,22 @@ def help_command():
 
 def new_command(user_input):
     print('Creating a new project...')
-    cursor.execute('CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY, name TEXT, directory TEXT)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY, name TEXT, directory TEXT, link TEXT)')
     parsed_input = user_input.split(' ')
     project_name = parsed_input[2]
-    working_directory = parsed_input[4]
+    if parsed_input[3] == '-dir':
+        working_directory = parsed_input[4]
+    elif parsed_input[3] == '-link' and len(parsed_input) <= 4:
+        project_link = parsed_input[4]
+        working_directory = 'None'
+    if len(parsed_input) >= 5:
+        if parsed_input[5] == '-dir':
+            working_directory = parsed_input[6]
+        elif parsed_input[5] == '-link':
+            project_link = parsed_input[6]
     print(f'Project name: {project_name}')
     print(f'Working directory: {working_directory}')
+    print(f'Project link: {project_link}')
     print('Please confirm the details above are correct.')
     confirm = False;
     while confirm == False:
@@ -48,8 +62,12 @@ def new_command(user_input):
 
         if confirm == 'Y':
             print('Adding project to database...')
-            cursor.execute('INSERT INTO projects (name, directory) VALUES (?, ?)', (project_name, working_directory))
-            connection.commit()
+            try:
+                cursor.execute('INSERT INTO projects (name, directory, link) VALUES (?, ?, ?)', (project_name, working_directory, project_link))
+                connection.commit()
+            except:
+                print('Error: Unable to add project to database.')
+                return
             print('... done!')
             confirm = True
         elif confirm == 'N':
@@ -64,7 +82,7 @@ def todo_command():
     try:
         cursor.execute('SELECT * FROM projects')
         projects = cursor.fetchall()
-        print(tabulate.tabulate(projects, headers=['ID', 'Name', 'Directory'], tablefmt='grid'))
+        print(tabulate.tabulate(projects, headers=['ID', 'Name', 'Directory', 'link'], tablefmt='grid'))
         print('... done!')
     except:
         print('No projects found, please create a new project.')
@@ -83,7 +101,7 @@ def finish_command(user_input):
             if confirm == 'Y':
                 print('Removing project from database...')
                 ##create a finished projects table if doesn't exist
-                cursor.execute('CREATE TABLE IF NOT EXISTS finished_projects (id INTEGER PRIMARY KEY, name TEXT, directory TEXT)')
+                cursor.execute('CREATE TABLE IF NOT EXISTS finished_projects (id INTEGER PRIMARY KEY, name TEXT, directory TEXT, link TEXT)')
                 ##move the project to the finished projects table
                 cursor.execute('INSERT INTO finished_projects SELECT * FROM projects WHERE id = ?', (project_id,))
                 cursor.execute('DELETE FROM projects WHERE id = ?', (project_id,))
@@ -123,6 +141,36 @@ def finish_command(user_input):
             else:
                 print('Invalid input, please try again.')
 
+def open_in_safari(project_id):
+    cursor.execute('SELECT * FROM projects WHERE id = ?', (project_id,))
+    project = cursor.fetchone()
+    print(f'Opening project {project[1]}...')
+    print(f'Working directory: {project[2]}')
+    print(f'Project link: {project[3]}')
+    os.system(f'cd')
+    try:
+        os.system(f'open -a Safari {project[3]}')
+    except:
+        print('Error: Unable to open project link.')
+        return
+    
+    print('... done!')
+
+def open_in_finder(project_id):
+    cursor.execute('SELECT * FROM projects WHERE id = ?', (project_id,))
+    project = cursor.fetchone()
+    print(f'Opening project {project[1]}...')
+    print(f'Working directory: {project[2]}')
+    print(f'Project link: {project[3]}')
+    os.system(f'cd')
+    try:
+        os.system(f'open {project[2]}')
+    except:
+        print('Error: Unable to open project dir.')
+        return
+
+    print('done...!')
+
 def open_command(user_input):
     ##user can open a project using id or name
     print('Opening project...')
@@ -136,20 +184,22 @@ def open_command(user_input):
             confirm = input('Y/N')
 
             if confirm == 'Y':
-                print('Opening project...')
-                cursor.execute('SELECT * FROM projects WHERE id = ?', (project_id,))
-                project = cursor.fetchone()
-                print(f'Opening project {project[1]}...')
-                print(f'Working directory: {project[2]}')
-                os.system(f'cd')
-                try:
-                    os.system(f'open {project[2]}')
-                except:
-                    print('Error: Unable to open project.')
-                    return
+                choice = False
+                while choice is False:
+                    print('Where would you like to open the project?')
+                    print('1. Finder')
+                    print('2. Safari')
+                    open_location = -1
+                    open_location = input('Enter a number: ')
+                    if open_location == '1':
+                        open_in_finder(project_id)
+                    elif open_location == '2':
+                        open_in_safari(project_id)
+                    else:
+                        print('Invalid input, please try again.')     
 
-                print('... done!')
                 confirm = True
+
             elif confirm == 'N':
                 print('Canceling project opening...')
                 print('... Canceled!')
@@ -166,19 +216,20 @@ def open_command(user_input):
             confirm = input('Y/N')
 
             if confirm == 'Y':
-                print('Opening project...')
-                cursor.execute('SELECT * FROM projects WHERE name = ?', (project_name,))
-                project = cursor.fetchone()
-                print(f'Opening project {project[1]}...')
-                print(f'Working directory: {project[2]}')
-                os.system(f'cd')
-                try:
-                    os.system(f'open {project[2]}')
-                except:
-                    print('Error: Unable to open project.')
-                    return             
-                print('... done!')
-                confirm = True
+                choice = False
+                while choice is False:
+                    print('Where would you like to open the project?')
+                    print('1. Finder')
+                    print('2. Safari')
+                    open_location = -1
+                    open_location = input('Enter a number: ')
+                    if open_location == '1':
+                        open_in_finder(project_id=project_id)
+                    elif open_location == '2':
+                        open_in_safari(project_id=project_id)
+                    else:
+                        print('Invalid input, please try again.')  
+
             elif confirm == 'N':
                 print('Canceling project opening...')
                 print('... Canceled!')
