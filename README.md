3c7641d2d75b0cc7e75fa7e0f410540ab8e72b10
jon429r <jonathanday088@gmail.com>
2023-11-29 10:37:28 -0500

    Commit
    redid some of the code and added an open function to open the file in finder.
    Also Made it so you can use the project id no name in some cases

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # project-finder
+
+Two day project I did to keep track of projects I am working on and their file locations
--- main.py
+++ main.py
@@ -1,62 +1,67 @@
 import sqlite3
 import tabulate
+import os
+
+print('Welcome to the project manager!')
 
 ##take in data from the user and pass it to the database
 connection = sqlite3.connect('database.db')
 cursor = connection.cursor()
 
 
-print('Welcome to the project manager!')
-exit = False
-while exit == False:    
+def help_command():
+    help_message = """
+    List of possible commands: \n
+    1. Create a new project: new -name <"project_name"> -dir <"working_directory"> \n
+    2. View existing projects: todo \n
+    3. Finished an existing project: finish -name <project_name> \n
+    You can also remove a project by typing finish -id <project_id> \n
+    4. Exit \n
+    5. Help \n
+    """
 
-    print('Please select an option:')
-    print('1. Create a new project: new -name <"project_name"> -dir <"working_directory">')
-    print('2. View existing projects: todo')
-    print('3. Finished an existing project: finish -name <project_name>')
-    print('3. Exit')
+    print(help_message)
 
-    user_input = input('Enter a command: ')
+def new_command(user_input):
+    print('Creating a new project...')
+    cursor.execute('CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY, name TEXT, directory TEXT)')
+    parsed_input = user_input.split(' ')
+    project_name = parsed_input[2]
+    working_directory = parsed_input[4]
+    print('... done!')
+    print(f'Project name: {project_name}')
+    print(f'Working directory: {working_directory}')
+    print('Please confirm the details above are correct.')
+    confirm = False;
+    while confirm == False:
+        confirm = input('Y/N')
 
-    if user_input.startswith('new'):
-        print('Creating a new project...')
-        cursor.execute('CREATE TABLE IF NOT EXISTS projects (id INTEGER PRIMARY KEY, name TEXT, directory TEXT)')
-        parsed_input = user_input.split(' ')
-        project_name = parsed_input[2]
-        working_directory = parsed_input[4]
-        print('... done!')
-        print(f'Project name: {project_name}')
-        print(f'Working directory: {working_directory}')
-        print('Please confirm the details above are correct.')
-        confirm = False;
-        while confirm == False:
-            confirm = input('Y/N')
+        if confirm == 'Y':
+            print('Adding project to database...')
+            cursor.execute('INSERT INTO projects (name, directory) VALUES (?, ?)', (project_name, working_directory))
+            connection.commit()
+            print('... done!')
+            confirm = True
+        elif confirm == 'N':
+            print('Canceling project creation...')
+            print('... Canceled!')
+            confirm = True
+        else:
+            print('Invalid input, please try again.')
 
-            if confirm == 'Y':
-                print('Adding project to database...')
-                cursor.execute('INSERT INTO projects (name, directory) VALUES (?, ?)', (project_name, working_directory))
-                connection.commit()
-                print('... done!')
-                confirm = True
-            elif confirm == 'N':
-                print('Canceling project creation...')
-                print('... Canceled!')
-                confirm = True
-            else:
-                print('Invalid input, please try again.')
+def todo_command():
+    print('Viewing existing projects...')
+    cursor.execute('SELECT * FROM projects')
+    projects = cursor.fetchall()
+    print(tabulate.tabulate(projects, headers=['ID', 'Name', 'Directory']))
+    print('... done!')
 
-    if user_input.startswith('todo'):
-        print('Viewing existing projects...')
-        ##display table of projects using tabulate
-        cursor.execute('SELECT * FROM projects')
-        results = cursor.fetchall()
-        print(tabulate.tabulate(results, headers=['id', 'Project', 'Directory'], tablefmt='grid'))
-
-    if user_input.startswith('finish'):
-        parsed_input = user_input.split(' ')
-        project_name = parsed_input[2]
-        print('Finishing project...')
-        print(f'Project name: {project_name}')
+def finish_command(user_input):
+    print('Finishing project...')
+    parsed_input = user_input.split(' ')
+    if parsed_input[1] == '-id':
+        project_id = parsed_input[2]
+        print(f'Project ID: {project_id}')
         print('Please confirm the details above are correct.')
         confirm = False;
         while confirm == False:
@@ -64,8 +69,8 @@ while exit == False:
 
             if confirm == 'Y':
                 print('Removing project from database...')
-                cursor.execute('DELETE FROM projects WHERE name = ?', (project_name,))
-                cursor.commit()
+                cursor.execute('DELETE FROM projects WHERE id = ?', (project_id,))
+                connection.commit()
                 print('... done!')
                 confirm = True
             elif confirm == 'N':
@@ -75,8 +80,106 @@ while exit == False:
             else:
                 print('Invalid input, please try again.')
 
-    if user_input.startswith('exit'):
-        print('Exiting...')
-        exit = True
+    elif parsed_input[1] == '-name':
+        project_name = parsed_input[2]
+        print(f'Project name: {project_name}')
+        print('Please confirm the details above are correct.')
+        confirm = False;
+        while confirm == False:
+            confirm = input('Y/N')
 
-print('Come again!')
+            if confirm == 'Y':
+                print('Removing project from database...')
+                cursor.execute('DELETE FROM projects WHERE name = ?', (project_name,))
+                connection.commit()
+                print('... done!')
+                confirm = True
+            elif confirm == 'N':
+                print('Canceling project deletion...')
+                print('... Canceled!')
+                confirm = True
+            else:
+                print('Invalid input, please try again.')
+
+def open_command(user_input):
+    ##user can open a project using id or name
+    print('Opening project...')
+    parsed_input = user_input.split(' ')
+    if parsed_input[1] == '-id':
+        project_id = parsed_input[2]
+        print(f'Project ID: {project_id}')
+        print('Please confirm the details above are correct.')
+        confirm, deny = False;
+        while confirm or deny is False:
+            confirm = input('Y/N')
+
+            if confirm == 'Y':
+                print('Opening project...')
+                cursor.execute('SELECT * FROM projects WHERE id = ?', (project_id,))
+                project = cursor.fetchone()
+                print(f'Opening project {project[1]}...')
+                print(f'Working directory: {project[2]}')
+                os.system(f'cd')
+                os.system(f'open {project[2]}')
+
+                print('... done!')
+                confirm = True
+            elif confirm == 'N':
+                print('Canceling project opening...')
+                print('... Canceled!')
+                deny = True
+            else:
+                print('Invalid input, please try again.')
+
+    elif parsed_input[1] == '-name':
+        project_name = parsed_input[2]
+        print(f'Project name: {project_name}')
+        print('Please confirm the details above are correct.')
+        confirm = False;
+        while confirm == False:
+            confirm = input('Y/N')
+
+            if confirm == 'Y':
+                print('Opening project...')
+                cursor.execute('SELECT * FROM projects WHERE name = ?', (project_name,))
+                project = cursor.fetchone()
+                print(f'Opening project {project[1]}...')
+                print(f'Working directory: {project[2]}')
+                os.system(f'cd')
+                os.system(f'open {project[2]}')
+                print('... done!')
+                confirm = True
+            elif confirm == 'N':
+                print('Canceling project opening...')
+                print('... Canceled!')
+                confirm = True
+            else:
+                print('Invalid input, please try again.')
+
+def main():
+    exit = False
+    while exit == False:    
+
+        user_input = input('Enter a command: ')
+
+        if user_input.startswith('help'):
+            help_command()
+
+        if user_input.startswith('new'):
+            new_command(user_input)
+
+        if user_input.startswith('todo'):
+            todo_command()
+
+        if user_input.startswith('finish'):
+            finish_command(user_input)
+
+        if user_input.startswith('open'):
+            open_command(user_input)
+
+        else:
+            print('Invalid command, please try again. Type "help" for a list of commands.')
+
+    print('Come again!')
+
+main()
